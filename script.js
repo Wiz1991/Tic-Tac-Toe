23d281d87c3c7bf4eae1a48313accc77ad664274
Wiz1991 <wizoftime92@gmail.com>
2020-09-10 23:14:02 +0200

    Player turns and cell placing

--- script.js
+++ script.js
@@ -1,81 +1,138 @@
+function createPlayer(name) {
 
-function createPlayer(name,symbol){
-    this.name = name
-    this.symbol = symbol
-
-    function getName(){
-        return this.name;
-    }
-    function getSymbol(){
-        return this.symbol
+    function getName() {
+        return name
     }
 
-    return{
-        getName,
-        getSymbol
+    return {
+        getName
     }
 }
-const gameBoard = (function(){
+const gameBoard = (function () {
     const board = new Array(9).fill('')
-    
+    const cells = document.querySelectorAll('.cell')
+
     const getBoard = () => board
 
+    const enableBoard = () => {
+        [...cells].forEach(cell => {
+            cell.addEventListener('click', play)
+        });
+    }
 
+    const play = (e) => {
+        let index = e.target.dataset.index
+        if (board[index] === '') {
+            board[index] = game.getTurn()
+            console.log(board)
+            render()
+            game.toggleTurn()
+        }
 
+    }
 
-    return{
-        getBoard
+    const render = () => {
+     
+
+        [...cells].forEach((cell,index) => {
+            if(board[index] === 'X'){
+                cell.classList.add("X")
+                cell.innerHTML = "X"
+            }
+            else if(board[index] === 'O'){
+                cell.classList.add("O")
+                cell.innerHTML = "O"
+            }
+        });
+
+    }
+
+    return {
+        getBoard,
+        enableBoard
     }
 })()
-const displayControler = (function(){
+const displayControler = (function () {
     const setup_section = document.getElementById('setup-section')
     const game_section = document.getElementById('game-section')
     const p_turn_name = document.getElementById('player-turn')
+    const turn_indicator = document.querySelector('.turn-indicator')
 
     const showSetupSection = () => {
         setup_section.style.display = "block"
     }
     const hideSetupSection = () => {
-        setup_section.style.display = "hidden"
+        setup_section.style.display = "none"
     }
 
     const showGameSection = () => {
         game_section.style.display = "block"
     }
     const hideGameSection = () => {
-        game_section.style.display = "hidden"
+        game_section.style.display = "none"
     }
 
-    const displayTurn = (name) => {
-        p_turn_name.innerHTML = name
-        //add player class to indicator based on turn
+    const displayTurn = (player) => {
+        p_turn_name.innerHTML = player.getName()
+        turn_indicator.classList.remove(`${game.getTurn() === 'X' ? 'player2' : 'player1' }`)
+        turn_indicator.classList.add(`${game.getTurn() === 'X' ? 'player1' : 'player2' }`)
     }
-    
 
 
-    return{
+
+    return {
         showGameSection,
         hideGameSection,
         showSetupSection,
         hideSetupSection,
         displayTurn
     }
-    
+
 })()
 
-const game = (function(){
+const game = (function () {
+    let player1 = {}
+    let player2 = {}
+    let atTurn = 'X'
     const form = document.getElementById('game-start')
 
+    const init = () => {
+        form.addEventListener('submit', startGame)
+    }
 
-    const init = function() {
-        
+    const getTurn = () => {
+        return atTurn
+    }
+    const toggleTurn = () => {
+        atTurn = atTurn == 'X' ? 'O' : 'X'
+        displayControler.displayTurn(atTurn == 'X' ? player1 : player2)
+    }
+
+    const startGame = function (e) {
+        e.preventDefault()
+        const name1 = document.getElementById('p1name').value
+        const name2 = document.getElementById('p2name').value
+        player1 = createPlayer(name1)
+        player2 = createPlayer(name2)
+
+        displayControler.hideSetupSection()
+        displayControler.showGameSection()
+
+        displayControler.displayTurn(player1)
+        gameBoard.enableBoard()
+
+    }
+
+
+    return {
+        init,
+        getTurn,
+        toggleTurn
     }
 
 
 
-
-
-
-
-
 })()
+
+
+game.init()
